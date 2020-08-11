591d7f91b4447329e7311d34ad9907aab1e1122b
Angelo Veltens <angelo.veltens@online.de>
2020-08-11 08:38:50 +0200

    install jest, prettier, eslint

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,36 @@
-import { icons, ns } from 'solid-ui'
+import { icons, ns } from "solid-ui";
 
 export const NotePane = {
-    global: false,
+  global: false,
 
-    icon: icons.iconBase + 'noun_15695.svg',
+  icon: icons.iconBase + "noun_15695.svg",
 
-    name: 'profile',
+  name: "profile",
 
-    label: function (subject, context) {
-        const t = context.session.store.findTypeURIs(subject)
-        if (
-            t[ns.as('Note').uri]
-        ) {
-            return 'Note'
-        }
-        return null
-    },
-
-    render: (subject, context) => {
-
-        const store = context.session.store
-        const content = store.any(subject, ns.as('content'));
-
-        const element = document.createElement('div');
-
-        const heading = document.createElement('h2');
-        const text = document.createTextNode("This is a as:Note")
-        heading.appendChild(text)
-
-        const contentDiv = document.createElement('div');
-        const contentText = document.createTextNode(content)
-        contentDiv.appendChild(contentText)
-
-        element.appendChild(heading)
-        element.appendChild(contentDiv)
-        return element;
+  label: function (subject, context) {
+    const t = context.session.store.findTypeURIs(subject);
+    if (t[ns.as("Note").uri]) {
+      return "Note";
     }
-}
+    return null;
+  },
 
+  render: (subject, context) => {
+    const store = context.session.store;
+    const content = store.any(subject, ns.as("content"));
+
+    const element = document.createElement("div");
+
+    const heading = document.createElement("h2");
+    const text = document.createTextNode("This is a as:Note");
+    heading.appendChild(text);
+
+    const contentDiv = document.createElement("div");
+    const contentText = document.createTextNode(content);
+    contentDiv.appendChild(contentText);
+
+    element.appendChild(heading);
+    element.appendChild(contentDiv);
+    return element;
+  },
+};
