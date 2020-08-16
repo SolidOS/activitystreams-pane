e3a9627b23c10d9181c9b2a114a2eb37ddb1a56b
Angelo Veltens <angelo.veltens@online.de>
2020-08-16 13:14:49 +0200

    show relative and absolute date / time

--- src/note/FormattedDate.tsx
+++ src/note/FormattedDate.tsx
@@ -1,11 +1,27 @@
 import { ReactElement } from "react";
 import * as React from "react";
 
+import { format } from "timeago.js";
+
 interface Props {
   value: Date;
 }
 
+const twoDigitsDateTime = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+};
+
 export const FormattedDate = ({ value }: Props): ReactElement => {
   if (!value) return null;
-  return <p>{value.toLocaleDateString()}</p>;
+  const formatted = value.toLocaleDateString(undefined, twoDigitsDateTime);
+  const relative = format(value);
+  return (
+    <p>
+      {relative} · {formatted}
+    </p>
+  );
 };
