56d4decdbea3d3409e4bf790bb1a4965dfa24719
Angelo Veltens <angelo.veltens@online.de>
2020-08-16 09:01:55 +0200

    style using react-jss

--- src/note/NoteCard.tsx
+++ src/note/NoteCard.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
 
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  card: {
+    display: "flex",
+    flexWrap: "wrap",
+    borderRadius: "4px",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: "1em",
+    boxShadow: "0 1px 5px rgba(0,0,0,0.2)",
+    alignItems: "center",
+    transition: "all .25s ease-in-out",
+    width: "100%",
+  },
+});
+
 import { Note } from "./data";
 import { ReactElement } from "react";
 
-export const NoteCard = ({ content }: Note): ReactElement => (
-  <div>
-    <h2>Activity Streams Note:</h2>
-    <p>{content}</p>
-  </div>
-);
+export const NoteCard = ({ content }: Note): ReactElement => {
+  const classes = useStyles();
+  return (
+    <div className={classes.card}>
+      <p>{content}</p>
+    </div>
+  );
+};
