9ca49a86758da707d0812bd86d9bcdc4324d6f10
Angelo Veltens <angelo.veltens@online.de>
2020-08-18 21:54:29 +0200

    e2e test published date

--- e2e-tests/note-with-content.spec.ts
+++ e2e-tests/note-with-content.spec.ts
@@ -5,24 +5,42 @@ import { DataBrowserContext } from "pane-registry";
 const store = graph();
 
 describe("activitystreams-pane", () => {
-  describe("note with content", () => {
+  describe("GIVEN a note", () => {
     const noteTurtle = `
       @prefix : <#> .
       @prefix as: <https://www.w3.org/ns/activitystreams#> .
+      @prefix XML: <http://www.w3.org/2001/XMLSchema#>.
+
 
       :it a as:Note;
-          as:content "The content of the note" .
+          as:content "The content of the note" ;
+          as:published  "2020-08-18T21:40:52+0200"^^XML:dateTime .
     `;
 
     beforeEach(() => {
       parse(noteTurtle, store, "https://pod.example/note");
     });
 
-    it("should render note content", () => {
-      const result = pane.render(sym("https://pod.example/note#it"), {
-        session: { store },
-      } as DataBrowserContext);
-      expect(result).toHaveTextContent("The content of the note");
+    describe("WHEN the pane is rendered", () => {
+      let html;
+
+      beforeEach(() => {
+        html = pane.render(sym("https://pod.example/note#it"), {
+          session: { store },
+        } as DataBrowserContext);
+      });
+
+      it("THEN the content is shown", () => {
+        expect(html).toHaveTextContent("The content of the note");
+      });
+
+      it("AND the published date is shown in current locale", () => {
+        expect(html).toHaveTextContent(
+          new Date("2020-08-18").toLocaleDateString(undefined, {
+            month: "2-digit",
+          })
+        );
+      });
     });
   });
 });
