46d29246e51d53527de2f5ebf616e23af7b93f3e
Timea Turdean <timea.turdean@gmail.com>
2022-02-08 10:15:50 +0100

    updated storybook, updated solid-ui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss"],
   webpackFinal: async (config) => {
     config.externals = {
       fs: "null",
