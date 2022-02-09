5b2db83a3c96cfb764c511d76a299b32195e6e7f
Angelo Veltens <angelo.veltens@online.de>
2022-02-09 18:27:12 +0100

    merge existing e2e tests and split them from unit test run

--- e2e-tests/note-content-and-attribution.spec.ts
+++ e2e-tests/note-content-and-attribution.spec.ts
@@ -2,19 +2,19 @@ import { fetcher, graph, parse, sym } from "rdflib";
 import { default as pane } from "../src";
 import { DataBrowserContext } from "pane-registry";
 
-import { getByText, getByAltText, waitFor } from "@testing-library/react";
+import { getByText, getByAltText, waitFor, act } from "@testing-library/react";
 import { givenMolid } from "molid/lib/molid-jest";
 
 describe("note with fetching attribution", () => {
   let store;
-  beforeEach(() => {
+  beforeAll(() => {
     store = graph();
     store.fetcher = fetcher(store, {});
   });
 
   givenMolid("with a typical Solid WebID profile", (molid) => {
     describe("GIVEN note attributed to a typical solid WebID", () => {
-      beforeEach(() => {
+      beforeAll(() => {
         const noteTurtle = `
       @prefix : <#> .
       @prefix as: <https://www.w3.org/ns/activitystreams#> .
@@ -31,14 +31,28 @@ describe("note with fetching attribution", () => {
       describe("WHEN the pane is rendered", () => {
         let html;
 
-        beforeEach(() => {
-          html = pane.render(sym("https://pod.example/note#it"), {
-            session: { store },
-          } as DataBrowserContext);
+        beforeAll(() => {
+          act(() => {
+            html = pane.render(sym("https://pod.example/note#it"), {
+              session: { store },
+            } as DataBrowserContext);
+          });
         });
 
         it("THEN the person's WebID is shown while loading", () => {
           expect(html).toHaveTextContent(molid.uri("/profile/card#me"));
+        });
+
+        it("AND the content of the note is shown", () => {
+          expect(html).toHaveTextContent("The content of the note");
+        });
+
+        it("AND the published date is shown in current locale", () => {
+          expect(html).toHaveTextContent(
+            new Date("2020-08-18").toLocaleDateString(undefined, {
+              month: "2-digit",
+            })
+          );
         });
 
         it("AND the person's name is shown eventually", async () => {
