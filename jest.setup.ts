abccc57d61708d064fec6c19edcb8609bf717761
bourgeoa <alain.bourgeois10@gmail.com>
2023-12-04 18:34:06 +0100

    textEncoder/textDecoder

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+// Polyfill for encoding which isn't present globally in jsdom
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
