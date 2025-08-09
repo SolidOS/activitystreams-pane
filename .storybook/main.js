b4252a30b255fe5f7b7a016548edaf9462ce34d3
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-08-09 17:00:01 +0200

    updated storybook to 7.x

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,20 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+
   webpackFinal: async (config) => {
     config.externals = {
       fs: "null",
     };
     return config;
   },
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+
+  docs: {
+    autodocs: true
+  }
 };
