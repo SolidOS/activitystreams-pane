ded849c7139cf274105ef05bc48d933d1a124680
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-11-06 14:30:54 +0100

    added peer dep and updated configs accordingly

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironmentOptions: {
     customExportConditions: ['node']
   },
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { configFile: './babel.config.js' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
--- jest.setup.ts
+++ jest.setup.ts
@@ -3,5 +3,8 @@ import "@testing-library/jest-dom";
 // Polyfill for encoding which isn't present globally in jsdom
 import { TextEncoder, TextDecoder } from 'util';
 
+// Mock external dependencies that solid-logic expects
+jest.mock('$rdf', () => require('rdflib'), { virtual: true })
+
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
