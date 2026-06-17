0653d61aa5ae01ba6411150a52696f74ee5f499c
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-06-17 13:51:46 +0200

    fix test issues with new authn

--- babel.config.mjs
+++ babel.config.mjs
@@ -7,5 +7,8 @@ export default {
     }],
     '@babel/preset-typescript',
     '@babel/preset-react'
+  ],
+  plugins: [
+    'babel-plugin-transform-import-meta'
   ]
 }
--- jest.config.mjs
+++ jest.config.mjs
@@ -7,10 +7,12 @@ export default {
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { configFile: './babel.config.mjs' }],
   },
+  transformIgnorePatterns: ['/node_modules/(?!(?:@uvdsl/solid-oidc-client-browser)/)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '^SolidLogic$': 'solid-logic',
+    '^solid-logic$': '<rootDir>/../solid-logic/dist/index.js',
     '^\\$rdf$': 'rdflib'
   },
 }
