a2b65fe758ce9486d33088841bc07fa5be382038
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-06-18 12:40:05 +0200

    cleaned jest config & updated rdflib

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,7 +7,7 @@ export default {
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { configFile: './babel.config.mjs' }],
   },
-  transformIgnorePatterns: ['/node_modules/(?!(@uvdsl/solid-oidc-client-browser|solid-ui|uuid|@noble|@lit|@lit-labs|lit|lit-html|lit-element)(/|$))'],
+  transformIgnorePatterns: ['/node_modules/(?!(@uvdsl/solid-oidc-client-browser|uuid|@noble|@lit|@lit-labs|lit|lit-html|lit-element)(/|$))'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
